70227232b5571746cc774bea9256d1607e042ba0
ken-zio <63251574+ken-zio@users.noreply.github.com>
2020-04-19 14:33:42 -0500

    finished module 2 activities

--- Module-2_Activities/callbacks-and-events/solved/main.js
+++ Module-2_Activities/callbacks-and-events/solved/main.js
@@ -1,7 +1,16 @@
 const myFunction = function(event) {
   console.log('Hooray!!')
+
+
+  // CHALLENGE
+  document.querySelector('#display').innerText = 'I did it!';
 }
+
 
 // Use `document.querySelector()` to obtain a reference to the `#test` element.
 
+const testElement = document.querySelector('#test');
+
 // Add an event listener that triggers `myFunction` when the mouse enters the `#test` element.
+
+testElement.addEventListener('mouseenter', myFunction);
